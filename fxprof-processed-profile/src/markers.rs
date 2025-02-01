eb973a4eb471b9402d3678c90e9a321142133ea2
Markus Stange <markus.stange@gmail.com>
2025-01-31 20:46:18 -0500

    Use serde::ser::Serialize and serde::de::Deserialize instead of serde::Serialize/Deserialize, so that everything still works if something in the dependency tree turns on serde's derive feature.

--- fxprof-processed-profile/src/markers.rs
+++ fxprof-processed-profile/src/markers.rs
@@ -2,8 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use serde::ser::{SerializeMap, SerializeSeq};
-use serde::Serialize;
+use serde::ser::{Serialize, SerializeMap, SerializeSeq};
 use serde_derive::Serialize;
 
 use super::profile::StringHandle;
